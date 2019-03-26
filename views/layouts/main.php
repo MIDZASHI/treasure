e045092dac524452ab50f7381540c309a54196d6
MIDZASHI <47472822+MIDZASHI@users.noreply.github.com>
2019-03-26 20:36:55 +0300

    Initial commit

---
+++ views/layouts/main.php
@@ -0,0 +1,86 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\PublicAsset;
+
+PublicAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<nav class="navbar main-menu navbar-default">
+    <div class="container">
+        <div class="menu-content">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/"><img src="/public/images/logo.jpg" alt=""></a>
+            </div>
+
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                <ul class="nav navbar-nav text-uppercase">
+                    <li><a data-toggle="dropdown" class="dropdown-toggle" href="/">Home</a>
+                        
+                    </li>
+                </ul>
+                <div class="i_con">
+                    <ul class="nav navbar-nav text-uppercase">
+                        <li><a href="/site/login">Login</a></li>
+                        <li><a href="/site/signup">Register</a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+
+<?=$content?>
+
+
+<footer class="footer-widget-section">
+    <div class="footer-copy">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">&copy; 2018
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
